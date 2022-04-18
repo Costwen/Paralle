67c9188769dce9c92cee40e3a5fa5ea512447f47
“costwen <1148796268@qq.com>
2022-04-18 20:52:25 +0800

    feat:add paralle optimize

--- raw.cpp
+++ raw.cpp
@@ -1,3 +1,48 @@
+#pragma GCC optimize(1)
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+
 #include <iostream>
 #include <algorithm>
 #include <omp.h>
@@ -14,7 +59,7 @@ using namespace std;
 
 struct BeginAndNum
 {
-    int begin;
+    int begin=-1;
     unsigned num;
 };
 
@@ -36,6 +81,7 @@ BeginAndNum vtIndex[MAX_VT_NUM];
 
 char str[MAX_STRING_LENGTH];
 
+
 struct SubTree
 {
     int root;
@@ -43,6 +89,12 @@ struct SubTree
 } subTreeTable[MAX_STRING_LENGTH][MAX_STRING_LENGTH][MAX_VN_NUM];
 
 int subTreeNumTable[MAX_STRING_LENGTH][MAX_STRING_LENGTH];
+
+SubTree subTreeBuf[2][MAX_STRING_LENGTH];
+SubTree subTreeChild1;
+SubTree subTreeChild2;
+SubTree subTreeParent;
+int begin, end;
 
 int vn_num;
 int production2_num;
@@ -77,28 +129,25 @@ int main()
     printf("Sort Time: %.3lf\n", (end_time - start_time) / CLOCKS_PER_SEC);
     
     start_time = clock();
-    for (int i = 0; i < MAX_VT_NUM; i++)
-    {
-        vtIndex[i].begin = -1;
-        vtIndex[i].num = 0;
-    }
+
     for (int i = 0; i < production1_num; i++)
     {
         int t = production1[i].child;
         if (vtIndex[t].begin == -1)
             vtIndex[t].begin = i;
-        vtIndex[t].num++;
+        ++vtIndex[t].num;
     }
     end_time = clock();
     
     printf("Production1 Init Time: %.3lf\n", (end_time - start_time) / CLOCKS_PER_SEC);
     
     start_time = clock();
+    int begin, end;
     for (int i = 0; i < string_length; i++)
     {
         int t = str[i];
-        int begin = vtIndex[t].begin;
-        int end = begin + vtIndex[t].num;
+        begin = vtIndex[t].begin;
+        end = begin + vtIndex[t].num;
         for (int j = begin; j < end; j++)
         {
             SubTree subTree;
@@ -125,19 +174,10 @@ int main()
     printf("Sort Time: %.3lf\n", (end_time - start_time) / CLOCKS_PER_SEC);
     
     start_time = clock();
-
-    for (int i = 0; i < vn_num; i++)
-    {
-        for (int j = 0; j < vn_num; j++)
-        {
-            vnIndex[i][j].begin = -1;
-            vnIndex[i][j].num = 0;
-        }
-    }
     end_time = clock();
     printf("Init Time: %.3lf\n", (end_time - start_time) / CLOCKS_PER_SEC);
 
-    
+
     start_time = clock();
 
     for (int i = 0; i < production2_num; i++)
@@ -151,47 +191,46 @@ int main()
     end_time = clock();
     printf("Count Init Time: %.3lf\n", (end_time - start_time) / CLOCKS_PER_SEC);
     start_time = clock();
-
-    for (int len = 2; len <= string_length; len++)
+    int len, left, right, i1, i2, j, k, newTreeNum;
+    int curr, last, oldTreeNum, copy_len;
+    for (len = 2; len <= string_length; len++)
     {
-        for (int left = 0; left <= string_length - len; left++)
+        for (left = 0; left <= string_length - len; left++)
         {
-            SubTree subTreeBuf[2][MAX_STRING_LENGTH];
-            //memset(subTreeBuf, 0, sizeof subTreeBuf);
-            int curr = 0;
-            int last = 1;
-            int oldTreeNum = 0;
-            for (int right = left + 1; right < left + len; right++)
+            curr = 0;
+            last = 1;
+            oldTreeNum = 0;
+            for (right = left + 1; right < left + len; right++)
             {
-                //printf("[%d, %d] = [%d, %d] + [%d, %d]\n", left, left + len, left, right, right, left + len);
-                for (int i1 = 0; i1 < subTreeNumTable[left][right - 1]; i1++)
+                for (i1 = 0; i1 < subTreeNumTable[left][right - 1]; i1++)
                 {
-                    SubTree subTreeChild1 = subTreeTable[left][right - 1][i1];
-                    for (int i2 = 0; i2 < subTreeNumTable[right][left + len - 1]; i2++)
+                    subTreeChild1 = subTreeTable[left][right - 1][i1];
+                    for (i2 = 0; i2 < subTreeNumTable[right][left + len - 1]; i2++)
                     {
-                        SubTree subTreeChild2 = subTreeTable[right][left + len - 1][i2];
-                        int begin = vnIndex[subTreeChild1.root][subTreeChild2.root].begin;
-                        int end = begin + vnIndex[subTreeChild1.root][subTreeChild2.root].num;
+                        subTreeChild2 = subTreeTable[right][left + len - 1][i2];
+                        begin = vnIndex[subTreeChild1.root][subTreeChild2.root].begin;
+                        end = begin + vnIndex[subTreeChild1.root][subTreeChild2.root].num;
                         if (begin == end)
                         {
                             continue;
                         }
                         swap(last, curr);
-                        int newTreeNum = 0;
-                        int k = 0;
-                        for (int j = begin; j < end; j++)
+                        newTreeNum = 0;
+                        k = 0;
+                        for (j = begin; j < end; j++)
                         {
-                            SubTree subTreeParent;
                             subTreeParent.root = production2[j].parent;
                             subTreeParent.num = subTreeChild1.num * subTreeChild2.num;
+                            
                             while (k < oldTreeNum && subTreeParent.root > subTreeBuf[last][k].root)
                                 subTreeBuf[curr][newTreeNum++] = subTreeBuf[last][k++];
+
                             if (k < oldTreeNum && subTreeParent.root == subTreeBuf[last][k].root)
                                 subTreeParent.num += subTreeBuf[last][k++].num;
                             subTreeBuf[curr][newTreeNum++] = subTreeParent;
                         }
-                        while (k < oldTreeNum)
-                        {
+
+                        while (k < oldTreeNum){
                             subTreeBuf[curr][newTreeNum++] = subTreeBuf[last][k++];
                         }
                         oldTreeNum = newTreeNum;
