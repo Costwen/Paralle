77a16f2e11e09ce996d1ad4be3796bb7d3987db0
costwen <1148796268@qq.com>
2022-04-19 11:23:44 +0800

    feat: add paralle

--- MyCYK.cpp
+++ MyCYK.cpp
@@ -1,6 +1,8 @@
 #pragma GCC optimize(1)
 #pragma GCC optimize(2)
 #pragma GCC optimize(3)
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("inline")
 #pragma GCC optimize("-fgcse")
@@ -40,14 +42,15 @@
 #pragma GCC optimize("-ftree-switch-conversion")
 #pragma GCC optimize("-foptimize-sibling-calls")
 #pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
 #pragma GCC optimize("-fdelete-null-pointer-checks")
+#pragma GCC optimize("Ofast", "inline", "-ffast-math")
+#pragma GCC diagnostic error "-std=c++11"
+
 #include <iostream>
 #include <algorithm>
 #include <omp.h>
 #include <time.h>
 #include <memory.h>
-#include <unordered_map>
 using namespace std;
 
 #define MAX_PRODUCTION2_NUM 512
@@ -85,8 +88,8 @@ struct SubTree
     int root;
     unsigned num;
 };
-unordered_map<int, int> subTreeTable[MAX_STRING_LENGTH][MAX_STRING_LENGTH];
-
+int subTreeTable[MAX_STRING_LENGTH][MAX_STRING_LENGTH][MAX_VN_NUM];
+int TableSize[MAX_STRING_LENGTH][MAX_STRING_LENGTH][MAX_VN_NUM];
 int vn_num;
 int production2_num;
 int production1_num;
@@ -94,8 +97,6 @@ int string_length;
 
 int main()
 {
-    double start_time, end_time;
-    start_time = clock();
     freopen("input.txt", "r", stdin);
     scanf("%d\n", &vn_num);
     scanf("%d\n", &production2_num);
@@ -127,6 +128,7 @@ int main()
         for (int j = begin; j < end; j++)
         {
             subTreeTable[i][i][production1[j].parent] = 1;
+            TableSize[i][i][++TableSize[i][i][0]] = production1[j].parent;
         }
     }
     sort(production2, production2 + production2_num, [](const Production2& a, const Production2& b)
@@ -146,22 +148,26 @@ int main()
     }
     for (int len = 2; len <= string_length; len++)
     {
-        for (int left = 0; left + len - 1 < string_length; left++)
+        #pragma omp parallel for
+        for (int left = 0; left < string_length - len + 1; left++)
         {
             for (int right = left + 1; right < left + len; right++){
-                for (auto it1 = subTreeTable[left][right - 1].begin(); it1 != subTreeTable[left][right - 1].end(); it1++)
-                    for (auto it2 = subTreeTable[right][left + len - 1].begin(); it2 != subTreeTable[right][left + len - 1].end(); it2++){
+                for (int it1 = 1; it1 <= TableSize[left][right-1][0]; it1++)
+                    for (int it2 = 1; it2 <= TableSize[right][left+len-1][0]; it2++){
                         // 枚举所有的产生式
-                        int child1 = it1->first;
-                        int child2 = it2->first;
-                        if (vnIndex[child1][child2].num == 0)
+                        int A = TableSize[left][right-1][it1];
+                        int B = TableSize[right][left+len-1][it2];
+                        if (vnIndex[A][B].num == 0)
                             continue;
-                        int num1 = it1->second;
-                        int num2 = it2->second;
-                        int begin = vnIndex[child1][child2].begin; // 获取当前产生式的左边的范围
-                        int end = begin + vnIndex[child1][child2].num; 
+                        int num1 = subTreeTable[left][right-1][A];
+                        int num2 = subTreeTable[right][left+len-1][B];
+                        int begin = vnIndex[A][B].begin; // 获取当前产生式的左边的范围
+                        int end = begin + vnIndex[A][B].num; 
+
                         for (int i = begin; i < end; i++)
                         {
+                            if (!subTreeTable[left][left+len-1][production2[i].parent])
+                                TableSize[left][left+len-1][++TableSize[left][left+len-1][0]] = production2[i].parent;
                             subTreeTable[left][left+len-1][production2[i].parent] += num1 * num2;
                         }
                     }
@@ -170,8 +176,5 @@ int main()
     }
 
     printf("%u\n", subTreeTable[0][string_length - 1][0]);
-    end_time = clock();
-    printf("Total Time: %.3lf\n", (end_time - start_time) / CLOCKS_PER_SEC);
-    
     return 0;
 }
